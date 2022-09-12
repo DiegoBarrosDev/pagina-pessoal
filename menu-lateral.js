2be4dd2bedceae6afe22eb10da13afde6ee50910
DiegoBarrosDev <barros_diegobarros@hotmail.com>
2022-09-12 17:14:52 -0300

    Atualização do menu - versão mobile

--- menu-lateral.js
+++ menu-lateral.js
@@ -1,9 +1,18 @@
 const botaoMenu = document.querySelector('.cabecalho__menu');
 const menu = document.querySelector('.menu-lateral');
 
-
-
+let visivel = false
 botaoMenu.addEventListener('click', () => {
-    
-    menu.classList.toggle('menu-lateral--ativo');
+    if (visivel) {
+        menu.classList.remove('menu-lateral--ativo')
+    } else {
+        menu.classList.add('menu-lateral--ativo');
+    }
+    visivel = !visivel
+})
+
+botaoMenu.addEventListener('blur', () => {
+    setTimeout(function(){
+        menu.classList.remove('menu-lateral--ativo')
+    }, 10)
 })
